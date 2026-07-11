b2ac75bc19e2fe5e854c43182082055a02ec55b4
pristine-aftermarket <pristine.aftermarket@gmail.com>
2026-07-11 15:06:56 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Supra WA API',
+  description: 'API Kirim WhatsApp',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
